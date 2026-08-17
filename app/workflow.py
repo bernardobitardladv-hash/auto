b1a858165e52f8a3374e44f4e1c42187f611a35f
bernardobitardladv-hash <bernardobitardladv@gmail.com>
2026-08-17 20:47:42 -0300

    Adicionar persistência e workflow Bitrix

---
+++ app/workflow.py
@@ -0,0 +1,5 @@
+Get-Content: 
+Line |
+   2 |  Get-Content -Raw 'app/workflow.py'
+     |  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+     | Cannot find path 'C:\Users\User\Desktop\Escritório (Cérebro)\app\workflow.py' because it does not exist.
