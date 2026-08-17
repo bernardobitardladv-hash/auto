1e0fa5188b6e566178b9d07773f3be9903c23aad
bernardobitardladv-hash <bernardobitardladv@gmail.com>
2026-08-17 20:53:47 -0300

    Adicionar testes de aceitação das cadências

---
+++ tests/test_acceptance.py
@@ -0,0 +1,5 @@
+Get-Content: 
+Line |
+   2 |  Get-Content -Raw tests\test_acceptance.py
+     |  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+     | Cannot find path 'C:\Users\User\Desktop\Escritório (Cérebro)\tests\test_acceptance.py' because it does not exist.
