f08a84d9979ab4952bd0366628b12ce71b9b587f
bernardobitardladv-hash <bernardobitardladv@gmail.com>
2026-08-17 20:47:39 -0300

    Adicionar persistência e workflow Bitrix

---
+++ app/db.py
@@ -0,0 +1,5 @@
+Get-Content: 
+Line |
+   2 |  Get-Content -Raw 'app/db.py'
+     |  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+     | Cannot find path 'C:\Users\User\Desktop\Escritório (Cérebro)\app\db.py' because it does not exist.
